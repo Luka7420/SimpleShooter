afc5f57a086d742f8072b9521c086375711461fc
Luka7420 <luka21716@gmail.com>
2025-08-04 17:02:25 +0200

    Implement enhanced input system in ShooterCharacter and update configuration files

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "ShooterCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
@@ -15,6 +18,17 @@ AShooterCharacter::AShooterCharacter()
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Ensure we have a valid PlayerController
+	if(APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		// Check if the subsystem is valid
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// Add mapping context with priority 0
+			Subsystem->AddMappingContext(ShooterCharacterMappingContext, 0); 
+		}
+	}
 	
 }
 
@@ -30,5 +44,44 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Ensure PlayerInputComponent is of type UEnhancedInputComponent
+	// This is necessary to use the enhanced input system features
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Bind the Move action to the Move function
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AShooterCharacter::Move); 
+		// Bind the Look action to the Look function
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AShooterCharacter::Look); 
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &AShooterCharacter::Jump); 
+
+
+	}
+
 }
 
+void AShooterCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();// Get the 2D vector from the input action value
+	
+	const FVector Forward = GetActorForwardVector(); // Get the forward vector of the character
+	AddMovementInput(Forward, MovementVector.Y); // Add movement input in the forward direction based on the Y component of the input vector
+
+	const FVector Right = GetActorRightVector(); // Get the right vector of the character
+	AddMovementInput(Right, MovementVector.X); // Add movement input in the right direction based on the X component of the input vector
+}
+
+void AShooterCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookVector = Value.Get<FVector2D>(); // Get the 2D vector from the input action value for looking around
+	
+	AddControllerYawInput(LookVector.X); // Add yaw input based on the X component of the input vector
+	AddControllerPitchInput(LookVector.Y); // Add pitch input based on the Y component of the input vector
+}
+
+void AShooterCharacter::Jump(const FInputActionValue& Value)
+{
+	if (Value.Get<bool>())
+    {
+        ACharacter::Jump();
+    }
+}
--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -19,6 +23,18 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* ShooterCharacterMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* JumpAction;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -26,4 +42,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Jump(const FInputActionValue& Value);
 };
